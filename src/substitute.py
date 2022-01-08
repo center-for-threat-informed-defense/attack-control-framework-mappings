b11a642a8beffa1d169c452aa6f8434cb9b7a579
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2022-01-08 17:53:54 -0500

    substitute.py loads local ATT&CK data, remove object parsing (not necessary)

--- src/substitute.py
+++ src/substitute.py
@@ -1,16 +1,14 @@
 import json
 import os
-
-from stix2.v20 import Bundle
-import requests
+import uuid
 
 
 def save_bundle(bundle, path):
     """helper function to write a STIX bundle to file"""
     print(f"{'overwriting' if os.path.exists(path) else 'writing'} {path}... ", end="", flush=True)
     with open(path, "w", encoding="utf-8") as outfile:
-        bundle.fp_serialize(outfile, indent=4, sort_keys=True, ensure_ascii=False)
-    print("done!")
+        json.dump(bundle, outfile, indent=4, sort_keys=True, ensure_ascii=False)
+    print("done")
 
 
 def substitute(attack_bundle, controls_bundle, mappings_bundle, allow_unmapped=False):
@@ -21,49 +19,48 @@ def substitute(attack_bundle, controls_bundle, mappings_bundle, allow_unmapped=F
     from controls_bundle and mappings_bundle
     """
     # add attack data which are not mitigations or mitigation relationships
-    out_objects = list(filter(
-        lambda sdo: not (sdo["type"] == "course-of-action") and not
-        (sdo["type"] == "relationship" and sdo["relationship_type"] == "mitigates"),
-        attack_bundle.objects))
+    out_objects = [
+        sdo
+        for sdo in attack_bundle
+        if (sdo["type"] != "course-of-action" and
+            not (sdo["type"] == "relationship" and sdo["relationship_type"] == "mitigates"))
+    ]
     if allow_unmapped:  # add all controls
-        out_objects += controls_bundle.objects
+        out_objects.extend(controls_bundle)
     else:  # add only controls which have associated mappings
         used_ids = set()
-        for mapping in mappings_bundle.objects:
+        for mapping in mappings_bundle:
             used_ids.add(mapping["source_ref"])
-        out_objects += list(filter(lambda sdo: sdo["id"] in used_ids, controls_bundle.objects))
+        out_objects.extend([sdo for sdo in controls_bundle if sdo["id"] in used_ids])
     # add mappings
-    out_objects += mappings_bundle.objects
+    out_objects.extend(mappings_bundle)
 
-    return Bundle(*out_objects, allow_custom=True)
+    return {
+        "type": "bundle",
+        "id": f"bundle--{uuid.uuid4()}",
+        "spec_version": "2.0",
+        "objects": out_objects,
+    }
 
 
-def main(controls, mappings, domain, version, allow_unmapped, output):
-    if version != "v9.0":
-        controls = controls.replace("attack_9_0", f"ATT&CK-{version}")
-        mappings = mappings.replace("attack_9_0", f"ATT&CK-{version}")
-        output = output.replace("attack_9_0", f"ATT&CK-{version}")
-
-    print("downloading ATT&CK data... ", end="", flush=True)
-    url = f"https://raw.githubusercontent.com/mitre/cti/ATT%26CK-{version}/{domain}/{domain}.json"
-    attack_data = Bundle(
-        requests.get(url, verify=True).json()["objects"],
-        allow_custom=True
-    )
+def main(attack, controls, mappings, allow_unmapped, output):
+    print("loading ATT&CK data... ", end="", flush=True)
+    with open(attack, "r") as f:
+        attack = json.load(f)["objects"]
     print("done")
 
     print("loading controls framework... ", end="", flush=True)
     with open(controls, "r") as f:
-        controls = Bundle(json.load(f)["objects"], allow_custom=True)
+        controls = json.load(f)["objects"]
     print("done")
 
     print("loading mappings... ", end="", flush=True)
     with open(mappings, "r") as f:
-        mappings = Bundle(json.load(f)["objects"])
+        mappings = json.load(f)["objects"]
     print("done")
 
     print("substituting... ", end="", flush=True)
-    out_bundle = substitute(attack_data, controls, mappings, allow_unmapped)
+    out_bundle = substitute(attack, controls, mappings, allow_unmapped)
     print("done")
 
     save_bundle(out_bundle, output)
