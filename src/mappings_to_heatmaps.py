d573935e52aa0ad8d42769c725c3006f4e7f2c38
MarkDavidson <mdavidson@mitre-engenuity.org>
2022-01-26 11:19:53 -0500

    fix(build): use correct file path

--- src/mappings_to_heatmaps.py
+++ src/mappings_to_heatmaps.py
@@ -129,12 +129,13 @@ def to_technique_list(controls, mappings, attack, family_id_to_controls, family_
 def get_framework_overview_layers(controls, mappings, attack, domain, framework_name, version):
     """ingest mappings and controls and attack_data, and return an array of layer jsons for layers
      according to control family"""
+    dashed_framework = framework_name.replace('_', '-')
     # build list of control families
     family_id_to_controls, family_id_to_name, id_to_family = parse_family_data(controls)
 
     out_layers = [
         {
-            "outfile": f"{framework_name}-overview.json",
+            "outfile": f"{dashed_framework}-overview.json",
             "layer": create_layer(
                 f"{framework_name} overview",
                 f"{framework_name} heatmap overview of control mappings, where scores are "
@@ -253,6 +254,9 @@ def get_x_mitre(objects, object_type="course-of-action"):
 
 
 def main(framework, attack, controls, mappings, domain, version, output, clear, build_dir):
+    underscore_version = version.replace('v', '').replace('.', '_')
+    dashed_framework = framework.replace('_', '-')
+
     print("loading ATT&CK data... ", end="", flush=True)
     with open(attack, "r") as f:
         attack = json.load(f)["objects"]
@@ -301,8 +305,9 @@ def main(framework, attack, controls, mappings, domain, version, output, clear, 
             f"represent the mappings from ATT&CK to {framework}:",
             "",
         ]
+
         prefix = (f"https://raw.githubusercontent.com/center-for-threat-informed-defense/"
-                  f"attack-control-framework-mappings/main/frameworks/ATT&CK-{version}")
+                  f"attack-control-framework-mappings/main/frameworks/attack_{underscore_version}")
         nav_prefix = "https://mitre-attack.github.io/attack-navigator/#layerURL="
 
         for layer in layers:
